82449b7a0a9fba50c59b9bffdf369c56e5f1f9c7
mert <mertcelep@gmail.com>
2019-02-12 00:33:31 +0300

    added console app

---
+++ src/Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Lib;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                System.Console.WriteLine("Aranacak noktanın belirlenmesi için klavyeden 2 tane input giriniz.");
+                System.Console.WriteLine("Uygulamadan çıkmak için exit yazınız..\n");
+
+                var result = WhichPolygon.Find(ReadInput("x"), ReadInput("y"));
+                System.Console.WriteLine($"\nSonuç = {result ?? "Sonuç bulunamadı."}\n");
+            }
+        }
+
+        static double ReadInput(string label)
+        {
+            while (true)
+            {
+                System.Console.Write($"{label} giriniz (örn 38.77) : ");
+                var str = System.Console.ReadLine();
+
+                if (str == "exit")
+                    Environment.Exit(0);
+
+                if (double.TryParse(str, out var _result))
+                    return _result;
+
+                System.Console.Write("Geçersiz değer. ");
+            }
+        }
+    }
+}
